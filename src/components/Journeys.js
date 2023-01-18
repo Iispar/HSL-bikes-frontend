23ad5bf118a3353d0d37413776bab252884c535a
Iiro Partanen <iispar@utu.fi>
2023-01-18 12:12:04 +0200

    pagination added

--- src/components/Journeys.js
+++ src/components/Journeys.js
@@ -8,15 +8,49 @@ import ListJourneys from './listJourneys'
  */
 const Journeys = () => {
   const [journeys, setJourneys] = useState([])
+  const [page, setPage] = useState(1)
+  const [filterNow, setFilterNow] = useState(['limit=10'])
 
   useEffect(() => {
     bikeService.getAll()
       .then(journeysData => setJourneys(journeysData))
   }, [])
 
-  const changeFilter = () => {
+  /**
+   * method for changing the filter. And setting the journeydata as instructed by the filtering.
+   * @param {Array} filter
+   */
+  const changeFilter = (filter) => {
+    setFilterNow(['Departure_station_name=Viiskulma', 'limit=10'])
     bikeService.getFiltered(['Departure_station_name=Viiskulma', 'limit=10'])
       .then(filteredJourneys => setJourneys(filteredJourneys))
+  }
+
+  /**
+   * method for moving forwards and backwords pages of journeydata.
+   * @param {String} direction
+   * @param {Integer} page
+   * @returns set journeydata as filtered
+   */
+  const changePage = (direction, page) => {
+    if (direction === 'f') {
+      const filter = [...filterNow]
+      filter.push(`skip=${page * 10}`)
+      setPage(page + 1)
+      bikeService.getFiltered(filter)
+        .then(filteredJourneys => setJourneys(filteredJourneys))
+    } else {
+      const filter = [...filterNow]
+      if (page * 10 - 20 < 0) {
+        // error handling
+        console.log('too low')
+        return
+      }
+      filter.push(`skip=${page * 10 - 20}`)
+      setPage(page - 1)
+      bikeService.getFiltered(filter)
+        .then(filteredJourneys => setJourneys(filteredJourneys))
+    }
   }
 
   return (
@@ -26,6 +60,10 @@ const Journeys = () => {
         </div>
         <div className = "listOfJourneys-container">
         <ListJourneys journeys = {journeys} />
+        </div>
+        <div className = "pageing-container">
+        <button onClick = {() => changePage('b', page)}> previous </button>
+            <button onClick = {() => changePage('f', page)}> next </button>
         </div>
     </div>
   )
