0a4a7324173632df8ab9d8206b0db4330709df0b
Iiro Partanen <iispar@utu.fi>
2023-02-15 17:00:49 +0200

    sass for main file
    started converting the css to sass files and also changing the naming conventions to fit BEM

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,27 @@ import React from 'react';
 import Journeys from './components/Journeys';
 import Stations from './components/Stations';
 import Header from './components/Header';
-import './components/Grid.css';
+import './components/styles/main.css';
 import './components/JourneysStyle.css';
 import './components/StationsStyle.css';
 import './components/Filters.css';
 import './components/StationListStyle.css';
 import './components/HeaderStyle.css';
-import './components/css/SearchBar.css';
+import './components/styles/SearchBar.css';
 
 /**
  * Basic structure for the application. Used a grid layout but only have
  * need for the header and a main element that is split into two.
  */
 const App = () => (
-  <div className="grid-container">
-    <div className="header-container" id="header-container">
+  <div className="grid">
+    <div className="grid__header" id="header-container">
       <Header />
     </div>
-    <div className="right-container" id="right-container">
+    <div className="grid__right" id="right-container">
       <Journeys />
     </div>
-    <div className="left-container" id="left-container">
+    <div className="grid__left" id="left-container">
       <Stations />
     </div>
   </div>
